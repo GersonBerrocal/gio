43a052b8fccf8d0223c1c8144177ab473ec8ea6e
Gerson <gwrsonberrocal@gmail.com>
2021-10-26 14:00:40 -0500

    Input file lee el tipo de file, transforma el peso y regresa un objeto

--- src/script.js
+++ src/script.js
@@ -8,6 +8,8 @@ const inputFile = document.getElementById('inputFile')
 const fileName = document.getElementById('fileName')
 const countCell = document.getElementById('countCell')
 const countComputer = document.getElementById('countComputer')
+const btnSubir = document.getElementById('btnSubir')
+const fileInfo = document.getElementById('fileInfo')
 // eslint-disable-next-line no-undef
 const socket = io()
 
@@ -17,10 +19,50 @@ if (/android/i.test(navigator.userAgent)) {
 
 inputFile.addEventListener('change', () => {
   if (inputFile.files.length) {
-    const name = inputFile.files[0].name
-    fileName.textContent = name
+    btnSubir.removeAttribute('disabled')
+    const file = inputFile.files[0]
+    fileName.textContent = file.name
+    let measure = ''
+    let size = file.size
+    let type = file.type
+    if (size < 1000) measure = 'bytes'
+    else if (size < 1000000) {
+      measure = 'KB'
+      size = size / 1000
+    } else if (size < 1000000000) {
+      measure = 'MB'
+      size = size / 1000000
+    } else {
+      measure = 'GB'
+      size = size / 1000000000
+    }
+    if (/video/i.test(type)) type = 'video'
+    else if (/image/i.test(type)) type = 'imagen'
+    else if (
+      type == 'text/plain' ||
+      type == 'application/pdf' ||
+      /office/i.test(type)
+    )
+      type = 'documento'
+    else type = 'otros'
+
+    const fileData = {
+      name: file.name,
+      size: [size.toFixed(2), measure],
+      type: type
+    }
+    setFileInfo(fileData)
   }
 })
+function setFileInfo(fileData) {
+  fileInfo.innerHTML = `
+  <div class="my-3">  
+  <p><b>Nombre : </b>${fileData.name}</p>
+  <p><b>Size :</b>${fileData.size[0]} ${fileData.size[1]}</p>
+  <p><b>Tipo : </b>${fileData.type}</p>
+  </div>
+  `
+}
 
 socket.on('setCount', data => {
   countCell.textContent = data.countCell
