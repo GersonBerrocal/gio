f43613760946c4b09c827b435258034aba596400
Gerson <gwrsonberrocal@gmail.com>
2021-11-03 21:27:27 -0500

    Termino el listado de archivos presentes durante la execucion de la aplicacion

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const http = require('http')
 const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server)
+const multer = require('multer')
+const storage = multer.diskStorage({
+  destination: function (req, res, cb) {
+    cb(null, '/tmp')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+    cb(null, uniqueSuffix + '-' + file.originalname)
+  }
+})
+const upload = multer({ storage })
+let files = []
 
 const host = '192.168.100.4'
 const port = 8080
@@ -17,8 +29,22 @@ app.get('/', (req, res) => {
 app.get('/bulma/bulma.css', (req, res) => {
   res.sendFile(__dirname + '/node_modules/bulma/css/bulma.min.css')
 })
-app.get('/imagenes/imagen.jpg', (req, res) => {
-  res.sendFile('/tmp/user.jpg')
+app.get('api/files/download/:id', (req, res) => {
+  res.send('Dale umpirri')
+})
+app.post('/api/files/upload', upload.single('fileShare'), (req, res) => {
+  files.push({
+    name: req.body.name,
+    type: req.body.type,
+    size: req.body.size,
+    nameFile: req.file.filename,
+    id: files.length
+  })
+  io.emit('upload', { files })
+  res.json({ result: true })
+})
+app.get('/api/files', (req, res) => {
+  res.json({ files })
 })
 app.use(express.static('src'))
 
--- src/script.js
+++ src/script.js
@@ -4,6 +4,9 @@ const countCell = document.getElementById('countCell')
 const countComputer = document.getElementById('countComputer')
 const btnSubir = document.getElementById('btnSubir')
 const fileInfo = document.getElementById('fileInfo')
+const formFile = document.getElementById('formFile')
+const containerFiles = document.getElementById('containerFiles')
+let lastFileData = []
 // eslint-disable-next-line no-undef
 const socket = io()
 
@@ -14,38 +17,73 @@ inputFile.addEventListener('change', () => {
     btnSubir.removeAttribute('disabled')
     const file = inputFile.files[0]
     fileName.textContent = file.name
-    let measure = ''
-    let size = file.size
     let type = file.type
-    if (size < 1000) measure = 'bytes'
-    else if (size < 1000000) {
-      measure = 'KB'
-      size = size / 1000
-    } else if (size < 1000000000) {
-      measure = 'MB'
-      size = size / 1000000
-    } else {
-      measure = 'GB'
-      size = size / 1000000000
-    }
-    if (/video/i.test(type)) type = 'video'
-    else if (/image/i.test(type)) type = 'imagen'
-    else if (
-      type == 'text/plain' ||
-      type == 'application/pdf' ||
-      /office/i.test(type)
-    )
-      type = 'documento'
-    else type = 'otros'
-
+    let [size, measure] = verifySizeFile(file.size)
+    type = verifyTypeFile(type)
     const fileData = {
       name: file.name,
-      size: [size.toFixed(2), measure],
+      size: [size, measure],
       type: type
     }
+    lastFileData.push(fileData)
     setFileInfo(fileData)
   }
 })
+formFile.addEventListener('submit', async e => {
+  e.preventDefault()
+  if (!inputFile.files.length) return
+  const formData = new FormData()
+  formData.append('fileShare', formFile['file'].files[0])
+  formData.append('name', lastFileData[0].name)
+  formData.append('size', lastFileData[0].size)
+  formData.append('type', lastFileData[0].type)
+  const res = await fetch('/api/files/upload', {
+    method: 'POST',
+    body: formData
+  })
+  const resObject = await res.json()
+  console.log(resObject.result)
+  lastFileData = []
+  resetInputFile()
+})
+
+function resetInputFile() {
+  inputFile.value = null
+  fileInfo.innerHTML = ''
+  fileName.textContent = '...'
+  btnSubir.setAttribute('disabled', 'disabled')
+}
+
+function verifySizeFile(size) {
+  let measure = ''
+  if (size < 1000) measure = 'bytes'
+  else if (size < 1000000) {
+    measure = 'KB'
+    size = size / 1000
+  } else if (size < 1000000000) {
+    measure = 'MB'
+    size = size / 1000000
+  } else {
+    measure = 'GB'
+    size = size / 1000000000
+  }
+  return [size.toFixed(2), measure]
+}
+
+function verifyTypeFile(type) {
+  if (/video/i.test(type)) type = 'video'
+  else if (/image/i.test(type)) type = 'imagen'
+  else if (
+    type == 'text/plain' ||
+    type == 'application/pdf' ||
+    /office/i.test(type)
+  )
+    type = 'documento'
+  else type = 'otros'
+
+  return type
+}
+
 function setFileInfo(fileData) {
   fileInfo.innerHTML = `
   <div class="my-3">  
@@ -56,7 +94,33 @@ function setFileInfo(fileData) {
   `
 }
 
+async function getDataFiles() {
+  const res = await fetch('/api/files', { method: 'GET' })
+  const resJson = await res.json()
+  if (resJson.files.length < 1) return ''
+  setFiles(resJson.files)
+}
+
+function setFiles(arrayFiles) {
+  let template = ``
+  arrayFiles.forEach(file => {
+    template += `<a class="panel-block">
+                  <span class="panel-icon">
+                    <i class="fas fa-book" aria-hidden="true"></i>
+                  </span>
+                    ${file.name}
+                  </a>`
+  })
+  containerFiles.innerHTML = template
+}
+
 socket.on('setCount', data => {
   countCell.textContent = data.countCell
   countComputer.textContent = data.countComputer
 })
+
+socket.on('upload', data => {
+  setFiles(data.files)
+})
+
+getDataFiles()
